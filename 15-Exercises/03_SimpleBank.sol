ba47f933dd6198e68d4cf2691c5db9a647f312a4
Dinon Isaig <dinonbonagua@gmail.com>
2026-01-31 23:22:06 +0800

    Submitting simple bank.sol

--- 15-Exercises/03_SimpleBank.sol
+++ 15-Exercises/03_SimpleBank.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.20;
 
 contract SimpleBank {
     // TODO 1: Create a mapping to store the balance of each address
-    // mapping(address => ?) public balances;
+    // We map an address (user) to a uint256 (amount of Wei they own)
+    mapping(address => uint256) public balances;
+    
     address public immutable owner;
 
     constructor() payable {
@@ -12,22 +14,36 @@ contract SimpleBank {
 
     // TODO 2: Write a deposit function, require msg.value > 0
     function deposit() external payable {
-        // code here
+        // "require" checks a condition; if false, it reverts the transaction
+        require(msg.value > 0, "Deposit amount must be greater than 0");
+        
+        // Add the sent Ether (msg.value) to the sender's balance in our mapping
+        balances[msg.sender] += msg.value;
     }
 
     // TODO 3: Write a function to view a person's balance
     function balanceOf(address user) external view returns (uint256) {
-        // return ...
+        return balances[user];
     }
 
     // TODO 4: Write a function to withdraw all of your money
-    // Hint: get amount = balances[msg.sender] → set to 0 → transfer
     function withdraw() external {
-        // code here
+        // 1. Get the amount the user currently has
+        uint256 amount = balances[msg.sender];
+        require(amount > 0, "No funds to withdraw");
+
+        // 2. Set balance to 0 BEFORE sending money (prevents Re-entrancy attacks)
+        balances[msg.sender] = 0;
+
+        // 3. Send the Ether back to the user
+        // We use "call" because it is currently the safest way to send Ether
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        require(success, "Transfer failed");
     }
 
     // TODO 5: Write a function to view the total money in the contract
+    // This shows how much Ether is locked inside this smart contract in total
     function contractBalance() external view returns (uint256) {
-        // return ...
+        return address(this).balance;
     }
 }
